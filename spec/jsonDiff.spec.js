b63bf2761644a8295f911109295fe989513cb971
Multihuntr <Multihuntr@hotmail.com>
2015-01-25 10:13:52 +1100

    Make the tests easier to read
    Pressing enter is fun

--- spec/jsonDiff.spec.js
+++ spec/jsonDiff.spec.js
@@ -5,7 +5,12 @@
 var jsonDiff = require('../jsonDiff.js');
 
 describe("no difference", function() {
+	
+	
+	
     describe("primitive data types", function() {
+		
+		
         it("finds nothing at first level", function() {
             var one = { one: 1 };
             var two = { one: 1 };
@@ -17,12 +22,23 @@ describe("no difference", function() {
             var two = { one: 1, two: { secondone: 1 }, three: { secondone: { thirdone: 1 } } };
             expect(jsonDiff(one, two)).toEqual([]);
         });
+		
+		
     });
+	
+	
+	
 });
 
 describe("additions", function() {
+	
+	
+	
 	describe("primitive data types", function() {
+		
+		
 		describe("first level", function() {
+			
 			it("one addition", function() {
 				var one = { one: 1 };
 				var two = { one: 1, two: 2 };
@@ -34,10 +50,12 @@ describe("additions", function() {
 				var two = { one: 1, two: 2, three: 3 };
 				expect(jsonDiff(one,two)).toEqual([{ at: "two", wasAdded: 2 }, { at: "three", wasAdded: 3 }]);
 			});
+			
 		});
 		
 		
 		describe("deeper levels", function() {
+			
 			it("one addition", function() {
 				var one = { one: { secondone: { thirdone: 1 } } };
 				var two = { one: { secondone: { thirdone: 1, thirdtwo: 2 } } };
@@ -49,11 +67,19 @@ describe("additions", function() {
 				var two = { one: { secondone: { thirdone: 1, thirdtwo: 2, thirdthree: 3 } } };
 				expect(jsonDiff(one,two)).toEqual([{ at: "one.secondone.thirdtwo", wasAdded: 2 }, { at: "one.secondone.thirdthree", wasAdded: 3 }]);
 			});
+			
 		});
+		
+		
 	});
 	
+	
+	
     describe("objects", function() {
+		
+		
 		describe("first level", function() {
+			
 			it("one addition at first level", function() {
 				var one = { one: { secondone: 1 } };
 				var two = { one: { secondone: 1 }, two: { secondone: 1 } };
@@ -65,15 +91,26 @@ describe("additions", function() {
 				var two = { one: { secondone: 1 }, two: { secondone: 1 }, three: { secondone: 1 } };
 				expect(jsonDiff(one,two)).toEqual([{ at: "two", wasAdded: { secondone: 1 } }, { at: "three", wasAdded: { secondone: 1 } }]);
 			});
+			
 		});
+		
+		
     });
+	
+	
+	
 });
 
 
 
 describe("removals", function() {
+	
+	
+	
     describe("primitive data types", function() {
+		
 		describe("first level", function() {
+			
 			it("one removal at first level", function() {
 				var one = { one: 1, two: 2 };
 				var two = { one: 1 };
@@ -85,12 +122,18 @@ describe("removals", function() {
 				var two = { one: 1 };
 				expect(jsonDiff(one,two)).toEqual([{ at: "two", wasRemoved: 2 }, { at: "three", wasRemoved: 3 }]);
 			});
+			
 		});
+		
     });
 	
 	
+	
     describe("objects", function() {
+		
+		
 		describe("first level", function() {
+			
 			it("one removal at first level", function() {
 				var one = { one: { secondone: 1 }, two: { secondone: 1 } };
 				var two = { one: { secondone: 1 } };
@@ -102,6 +145,13 @@ describe("removals", function() {
 				var two = { one: { secondone: 1 } };
 				expect(jsonDiff(one,two)).toEqual([{ at: "two", wasRemoved: { secondone: 1 } }, { at: "three", wasRemoved: { secondone: 1 } }]);
 			});
+			
 		});
+		
+		
     });
+	
+	
+	
+	
 });
